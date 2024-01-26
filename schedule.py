8cb2422f0c200d88795799ba46951246ac17b112
anshullamsonge <129319318+anshullamsonge@users.noreply.github.com>
2024-01-26 20:59:45 +0530

    Add files via upload

---
+++ schedule.py
@@ -0,0 +1,38 @@
+timespan=input("WHATS YOUR SCHEDULE...")
+if timespan==("8am to 9am"):
+    print("WAKEUP GE READY FOR COLLEGE")
+elif timespan==("9am to 10am"):
+       print("REACH COLLEGE AND GO FOR LECTURE")
+elif timespan==("10am to 11:30am"):
+    print("FIRST LECTURE")
+elif timespan==("11:30am to 12:00am"):
+    print("FIRST RECESS")
+elif timespan==("12:00pm to 2:00pm"):
+    print("NEXT LECTURE")
+elif timespan==("2:00pm to 2:15pm"):
+    print("SECOND RECESS")
+elif timespan==("2:15pm to 4:00pm"):
+    print("FOOTBALL PRACTICE")
+elif timespan==("4:00pm to 4:30pm"):
+    print("REACH HOME")
+elif timespan==("4:30pm to 5:00pm"):
+    print("REACH CLASS (CMGC)")
+elif timespan==("5:00pm to 7:00pm"):
+    print("CLASS WEB DEVELOPMENT")
+elif timespan==("7:00pm to 8:00pm"):
+    print("SHOP")
+elif timespan==("8:00pm to 8:30pm"):
+    print("REACH HOME FINALLY")
+elif timespan==("8:30pm to 9:00pm"):
+    print("DO HOMEWORK")
+elif timespan==("9:00pm to 9:30pm"):
+    print("DINNER")
+elif timespan==("9:30pm to 11:30pm"):
+    print("USE PHONE")
+elif timespan==("11:30pm to 12:00pm"):
+    print("SLEEP GOOD NIGHT")
+    
+
+    
+
+    
